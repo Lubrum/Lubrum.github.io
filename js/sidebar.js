7cf50ec644ebfd03074f83357f8831f2c35166d4
luciano <lucianobrum18@gmail.com>
2019-12-01 13:27:42 -0300

    bug fixes in front end, button to collapsed fixed sidebar, some changes in borders and sizes to work in firefox

---
+++ js/sidebar.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(window).resize(function() { 
+    if($(window).width() < 1400){
+      document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 3";
+    }
+    else{
+      if(document.getElementsByClassName("navigation-menu")[0].style.display != "none"){
+        document.getElementsByClassName("main-content")[0].style = "grid-column: 2 / 3";
+      }
+    }
+  });
+});
+
+function openNav() {
+  if(document.querySelector(".container").classList.contains("animate"))
+    document.querySelector(".container").classList.remove("animate");
+  else
+    document.querySelector(".container").classList.add("animate");
+  
+}
