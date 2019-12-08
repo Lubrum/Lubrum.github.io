f44b2f8ac5835d87008e9052fdd62e6fe20f5a04
luciano <lucianobrum18@gmail.com>
2019-12-07 21:19:11 -0300

    " Removed top button menu, font in menu options in header is responsive now "

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,11 +4,21 @@ $(document).ready(function () {
       document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 3";
     }
     else{
+      //document.getElementsByClassName("site-nav")[0].style = "max-width: none; max-height: none;";
       if(document.getElementsByClassName("navigation-menu")[0].style.display != "none"){
         document.getElementsByClassName("main-content")[0].style = "grid-column: 2 / 3";
       }
     }
   });
+/*
+  $("#nav-trigger").change( function(){
+    if( $(this).is(':checked') ) {
+      document.getElementsByClassName("site-nav")[0].style = "max-width: 25%; max-height: none;";
+    } 
+    else{
+      document.getElementsByClassName("site-nav")[0].style = "max-width: 10%; max-height: 90%;";
+    }
+ });*/
 });
 
 function openNav() {
