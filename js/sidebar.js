210ab62179c27e8bc51abaff28fa465785c85df0
Lubrum <lucianobrum18@gmail.com>
2020-04-20 03:08:31 -0300

    twitter and linkedin removed, better responsive layout, minor bug fixes

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(window).resize(function() { 
-    if($(window).width() < 1400){
+    if($(window).width() < 1200){
       document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 3";
     }
     else{
@@ -11,11 +11,14 @@ $(document).ready(function () {
   });
 });
 
-function openNav() {
-  if(document.querySelector(".container").classList.contains("without-sidebar"))
+function open_sidebar() {
+  if(document.querySelector(".container").classList.contains("without-sidebar")) {
     document.querySelector(".container").classList.remove("without-sidebar");
-  else
+    document.cookie = "without-sidebar=false";
+  } else {
     document.querySelector(".container").classList.add("without-sidebar");
+    document.cookie = "without-sidebar=true";
+  }
 }
 
 function getCook(cookiename) {
--- js/zoom.js
+++ js/zoom.js
@@ -1,3 +1,4 @@
+
 // sourcecode : https://activebridge.org/blog/image-zoom-on-pure-css //
 function zoom(e){
   var zoomer = e.currentTarget;
@@ -7,10 +8,68 @@ function zoom(e){
   y = offsetY / zoomer.offsetHeight * 100
   zoomer.style.backgroundPosition = x + '% ' + y + '%';
 }
+/*
+$('.page-link').click(function (e) {
+  var t = this,  href = t.href;
+  // prevent the default behavior //
+  e.preventDefault();
 
+  // allows websites to access and modify the browser’s history without loading any pages //
+  history.pushState(null, null, t.href);
+
+  changePage2();
+
+});
+
+var html = document.querySelector('html');
+
+function changePage2() {
+  // Note, the URL has already been changed
+  var url = window.location.href;
+
+  loadPage(url).then(function(responseText) {
+    var wrapper = document.createElement('html');
+    wrapper.innerHTML = responseText;
+
+    var oldContent = document.querySelector('body');
+    //var oldScript = document.querySelector('#fb-script');
+    //var oldContainer = document.querySelector('.container');
+    var newContent = wrapper.querySelector('body');
+
+    //var scriptElement = document.createElement('script');
+    //scriptElement.id = "fb-script"
+    //scriptElement.type = 'text/javascript';
+    //scriptElement.src = "https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v4.0&appId=2337139719936629&autoLogAppEvents=1";
+
+    html.appendChild(newContent);
+    //body.appendChild(scriptElement);
+
+    animate2(oldContent, newContent);
+  });
+}
+
+function animate2(oldContent, newContent) {
+  
+  var fadeOut = oldContent.className += " " + "animated fadeOut";
+  
+  setTimeout(function (){ 
+    oldContent.parentNode.removeChild(oldContent);
+    //oldScript.parentNode.removeChild(oldScript);
+  }, 1000);
+
+  var fadeIn = newContent.className += " " + "animated fadeIn";
+  
+  setTimeout(function (){ 
+    newContent.classList.remove("animated");
+    newContent.classList.remove("fadeIn");
+    //if($(window).width() >=  1400){
+     // $('.main-content').css('grid-column', '2 / 3');
+    //} 
+  }, 1000);
+}
+*/
 $('.post-link').click(function (e) {
   var t = this,  href = t.href;
-   
   // prevent the default behavior //
   e.preventDefault();
 
@@ -23,23 +82,22 @@ $('.post-link').click(function (e) {
 
 window.addEventListener('popstate', changePage);
 
-var cache = {};
 function loadPage(url) {
-  if (cache[url]) {
-    return new Promise(function(resolve) {
-      resolve(cache[url]);
-    });
-  }
-
   return fetch(url, {
     method: 'GET'
   }).then(function(response) {
-    cache[url] = response.text();
-    return cache[url];
+    return response.text();
   });
 }
 
 var body = document.querySelector('.container');
+
+function getCook(cookiename) {
+  
+  var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
+  return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
+  
+}
 
 function changePage() {
 
@@ -52,7 +110,6 @@ function changePage() {
 
     var oldContent = document.querySelector('.main-content');
     var oldContainer = document.querySelector('.container');
-    
 
     //if(oldContainer.classList.contains("without-sidebar")) {
      // document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 3";
@@ -61,26 +118,33 @@ function changePage() {
 
     var newContent = wrapper.querySelector('.main-content');
 
+    var scriptElement = document.createElement('script');
+    scriptElement.id = "fb-script"
+    scriptElement.type = 'text/javascript';
+    scriptElement.src = "https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v4.0&appId=2337139719936629&autoLogAppEvents=1";
+
     body.appendChild(newContent);
+    body.appendChild(scriptElement);
+
     animate(oldContent, newContent);
   });
 }
 
 function animate(oldContent, newContent) {
-  
+
   var fadeOut = oldContent.className += " " + "animated fadeOut";
   
   setTimeout(function (){ 
     oldContent.parentNode.removeChild(oldContent); 
-  }, 1200);
+  }, 2200);
 
   var fadeIn = newContent.className += " " + "animated fadeIn";
   
   setTimeout(function (){ 
     newContent.classList.remove("animated");
-    newContent.classList.remove("slideInRight");
+    newContent.classList.remove("fadeIn");
     if($(window).width() >=  1400){
       $('.main-content').css('grid-column', '2 / 3');
     } 
-  }, 800);
+  }, 1200);
 }
