f1d8380016bea7b232d3f12c07d561c076f7fe08
Lubrum <lucianobrum18@gmail.com>
2020-10-04 21:35:21 -0300

    Moving script out of head, to the end of body, in default include. Facebook share button removed. Removing css from html and moving to respective scss files. Minor changes in transitions js files.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'assets', 'js'),
     publicPath: '{{ site.url }}/assets/js/',
-    filename: 'main-transitions.js'
+    filename: 'main.js'
   },
   module: {
     rules: [
