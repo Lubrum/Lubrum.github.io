ef93eed87806803582315f15c8af724bf512659b
Lubrum <lucianobrum18@gmail.com>
2021-03-07 02:09:32 -0300

    Code improvements

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,15 +1,3 @@
-window.addEventListener('resize', function(event) {
-    if (window.innerWidth < 1200) {
-        document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 2";
-    } else {
-        if (document.getElementsByClassName("navigation-menu")[0].style.display != "none") {
-            document.getElementsByClassName("main-content")[0].style = "grid-column: 2 / 3";
-        } else {
-            document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 3";
-        }
-    }
-});
-
 function open_sidebar() {
     if (document.querySelector(".navigation-menu").classList.contains("element-ready")) {
         document.querySelector(".navigation-menu").classList.remove("element-ready")
@@ -25,7 +13,41 @@ function open_sidebar() {
     }
 }
 
+function twe_author_description() {  
+    
+    var app = document.getElementById('author-description');
+
+    var typewriter = new Typewriter(app, {
+      loop: false,
+      delay: 50,
+    });
+  
+    typewriter
+      .typeString('Engenheiro de Computação<br><br>')
+      .pauseFor(100)
+      .typeString('<strong>Mestre em Computação Aplicada</strong><br><br>')
+      .pauseFor(100)
+      .typeString('<strong>Desenvolvedor Backend/Mobile</strong><br><br>')
+      .pauseFor(100)
+      .typeString('Entusiasta da área de dados')
+      .pauseFor(100)
+      .start();
+  
+}
+
+function setOverflowX() {
+    let cosmosContainer = document.getElementsByClassName("cosmos-container")[0];
+    cosmosContainer.style.overflowX = "hidden";
+}
+
+function unsetOverflowX() {
+    let cosmosContainer = document.getElementsByClassName("cosmos-container")[0];
+    cosmosContainer.style.overflowX = "visible";
+}
+
 window.onload = function() {
+
+    twe_author_description();
 
     const sidebar = document.getElementsByClassName("navigation-menu")[0];
     sidebar.addEventListener("transitionstart", function(event) {
@@ -42,12 +64,14 @@ window.onload = function() {
     })
 }
 
-function setOverflowX() {
-    let cosmosContainer = document.getElementsByClassName("cosmos-container")[0];
-    cosmosContainer.style.overflowX = "hidden";
-}
-
-function unsetOverflowX() {
-    let cosmosContainer = document.getElementsByClassName("cosmos-container")[0];
-    cosmosContainer.style.overflowX = "visible";
-}
+window.addEventListener('resize', function(event) {
+    if (window.innerWidth < 1200) {
+        document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 2";
+    } else {
+        if (document.getElementsByClassName("navigation-menu")[0].style.display != "none") {
+            document.getElementsByClassName("main-content")[0].style = "grid-column: 2 / 3";
+        } else {
+            document.getElementsByClassName("main-content")[0].style = "grid-column: 1 / 3";
+        }
+    }
+});
